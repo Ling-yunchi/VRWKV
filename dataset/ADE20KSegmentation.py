18644c52deab76d240f7a30a38673fc6552a9d8a
Ling-yunchi <wr020210@gmail.com>
2024-09-16 17:24:52 +0800

    add ADE20K Dataset

---
+++ dataset/ADE20KSegmentation.py
@@ -0,0 +1,138 @@
+"""Pascal ADE20K Semantic Segmentation Dataset."""
+
+import os
+
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class ADE20KSegmentation(Dataset):
+    """ADE20K Semantic Segmentation Dataset.
+
+    Parameters
+    ----------
+    root : string
+        Path to VOCdevkit folder.
+    split: string
+        'train', 'val'
+    transform : callable, optional
+        A function that transforms the image and mask
+    """
+
+    NUM_CLASS = 150
+    # fmt: off
+    CLASSES = ("wall", "building, edifice", "sky", "floor, flooring", "tree",
+               "ceiling", "road, route", "bed", "windowpane, window", "grass",
+               "cabinet", "sidewalk, pavement",
+               "person, individual, someone, somebody, mortal, soul",
+               "earth, ground", "door, double door", "table", "mountain, mount",
+               "plant, flora, plant life", "curtain, drape, drapery, mantle, pall",
+               "chair", "car, auto, automobile, machine, motorcar",
+               "water", "painting, picture", "sofa, couch, lounge", "shelf",
+               "house", "sea", "mirror", "rug, carpet, carpeting", "field", "armchair",
+               "seat", "fence, fencing", "desk", "rock, stone", "wardrobe, closet, press",
+               "lamp", "bathtub, bathing tub, bath, tub", "railing, rail", "cushion",
+               "base, pedestal, stand", "box", "column, pillar", "signboard, sign",
+               "chest of drawers, chest, bureau, dresser", "counter", "sand", "sink",
+               "skyscraper", "fireplace, hearth, open fireplace", "refrigerator, icebox",
+               "grandstand, covered stand", "path", "stairs, steps", "runway",
+               "case, display case, showcase, vitrine",
+               "pool table, billiard table, snooker table", "pillow",
+               "screen door, screen", "stairway, staircase", "river", "bridge, span",
+               "bookcase", "blind, screen", "coffee table, cocktail table",
+               "toilet, can, commode, crapper, pot, potty, stool, throne",
+               "flower", "book", "hill", "bench", "countertop",
+               "stove, kitchen stove, range, kitchen range, cooking stove",
+               "palm, palm tree", "kitchen island",
+               "computer, computing machine, computing device, data processor, "
+               "electronic computer, information processing system",
+               "swivel chair", "boat", "bar", "arcade machine",
+               "hovel, hut, hutch, shack, shanty",
+               "bus, autobus, coach, charabanc, double-decker, jitney, motorbus, "
+               "motorcoach, omnibus, passenger vehicle",
+               "towel", "light, light source", "truck, motortruck", "tower",
+               "chandelier, pendant, pendent", "awning, sunshade, sunblind",
+               "streetlight, street lamp", "booth, cubicle, stall, kiosk",
+               "television receiver, television, television set, tv, tv set, idiot "
+               "box, boob tube, telly, goggle box",
+               "airplane, aeroplane, plane", "dirt track",
+               "apparel, wearing apparel, dress, clothes",
+               "pole", "land, ground, soil",
+               "bannister, banister, balustrade, balusters, handrail",
+               "escalator, moving staircase, moving stairway",
+               "ottoman, pouf, pouffe, puff, hassock",
+               "bottle", "buffet, counter, sideboard",
+               "poster, posting, placard, notice, bill, card",
+               "stage", "van", "ship", "fountain",
+               "conveyer belt, conveyor belt, conveyer, conveyor, transporter",
+               "canopy", "washer, automatic washer, washing machine",
+               "plaything, toy", "swimming pool, swimming bath, natatorium",
+               "stool", "barrel, cask", "basket, handbasket", "waterfall, falls",
+               "tent, collapsible shelter", "bag", "minibike, motorbike", "cradle",
+               "oven", "ball", "food, solid food", "step, stair", "tank, storage tank",
+               "trade name, brand name, brand, marque", "microwave, microwave oven",
+               "pot, flowerpot", "animal, animate being, beast, brute, creature, fauna",
+               "bicycle, bike, wheel, cycle", "lake",
+               "dishwasher, dish washer, dishwashing machine",
+               "screen, silver screen, projection screen",
+               "blanket, cover", "sculpture", "hood, exhaust hood", "sconce", "vase",
+               "traffic light, traffic signal, stoplight", "tray",
+               "ashcan, trash can, garbage can, wastebin, ash bin, ash-bin, ashbin, "
+               "dustbin, trash barrel, trash bin",
+               "fan", "pier, wharf, wharfage, dock", "crt screen",
+               "plate", "monitor, monitoring device", "bulletin board, notice board",
+               "shower", "radiator", "glass, drinking glass", "clock", "flag")
+    # fmt: on
+
+    def __init__(self, root, mode="train", transforms=None):
+        super(ADE20KSegmentation, self).__init__()
+        assert mode in ("train", "val")
+        self.root = root
+        self.mode = mode
+        self.transforms = transforms
+
+        self.images, self.masks = _get_ade20k_pairs(root, mode)
+        assert len(self.images) > 0, f"Found 0 images in subfolders of: {root}"
+        assert len(self.images) == len(
+            self.masks
+        ), "image and mask list length different."
+
+    def __getitem__(self, index):
+        img = Image.open(self.images[index]).convert("RGB")
+        mask = Image.open(self.masks[index])
+        if self.transforms is not None:
+            img = self.transforms(img, mask)
+        return img, mask
+
+    def __len__(self):
+        return len(self.images)
+
+    @property
+    def classes(self):
+        """Category names."""
+        return type(self).CLASSES
+
+
+def _get_ade20k_pairs(folder, mode="train"):
+    img_paths = []
+    mask_paths = []
+    if mode == "train":
+        img_folder = os.path.join(folder, "images/training")
+        mask_folder = os.path.join(folder, "annotations/training")
+    else:
+        img_folder = os.path.join(folder, "images/validation")
+        mask_folder = os.path.join(folder, "annotations/validation")
+    for filename in os.listdir(img_folder):
+        basename, _ = os.path.splitext(filename)
+        if filename.endswith(".jpg"):
+            imgpath = os.path.join(img_folder, filename)
+            maskname = basename + ".png"
+            maskpath = os.path.join(mask_folder, maskname)
+            if os.path.isfile(maskpath):
+                img_paths.append(imgpath)
+                mask_paths.append(maskpath)
+            else:
+                print("cannot find the mask:", maskpath)
+
+    return img_paths, mask_paths
